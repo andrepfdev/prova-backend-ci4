9cd8f9544f9ee2d043474c1390409cf9546f8518
andrepfdev <dreletras@gmail.com>
2025-02-27 18:55:53 -0300

    fix: rota group api

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,30 +7,32 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-// Rotas para CRUD de Produtos
-$routes->get('/produtos', 'ProdutoController::index');
-$routes->get('/produtos/(:num)', 'ProdutoController::show/$1');
-$routes->post('/produtos', 'ProdutoController::create');
-$routes->put('/produtos/(:num)', 'ProdutoController::update/$1');
-$routes->delete('/produtos/(:num)', 'ProdutoController::delete/$1');
+$routes->group('api', function ($routes) {
+    // Rotas para CRUD de Produtos
+    $routes->get('produtos', 'ProdutoController::index');
+    $routes->get('produtos/(:num)', 'ProdutoController::show/$1');
+    $routes->post('produtos', 'ProdutoController::create');
+    $routes->put('produtos/(:num)', 'ProdutoController::update/$1');
+    $routes->delete('produtos/(:num)', 'ProdutoController::delete/$1');
 
-// Rotas para CRUD de Clientes
-$routes->get('/clientes', 'ClienteController::index');
-$routes->get('/clientes/(:num)', 'ClienteController::show/$1');
-$routes->post('/clientes', 'ClienteController::create');
-$routes->put('/clientes/(:num)', 'ClienteController::update/$1');
-$routes->delete('/clientes/(:num)', 'ClienteController::delete/$1');
+    // Rotas para CRUD de Clientes
+    $routes->get('clientes', 'ClienteController::index');
+    $routes->get('clientes/(:num)', 'ClienteController::show/$1');
+    $routes->post('clientes', 'ClienteController::create');
+    $routes->put('clientes/(:num)', 'ClienteController::update/$1');
+    $routes->delete('clientes/(:num)', 'ClienteController::delete/$1');
 
-// Rotas para CRUD de Pedidos
-$routes->get('/pedidos', 'PedidoController::index');
-$routes->get('/pedidos/(:num)', 'PedidoController::show/$1');
-$routes->post('/pedidos', 'PedidoController::create');
-$routes->put('/pedidos/(:num)', 'PedidoController::update/$1');
-$routes->delete('/pedidos/(:num)', 'PedidoController::delete/$1');
+    // Rotas para CRUD de Pedidos
+    $routes->get('pedidos', 'PedidoController::index');
+    $routes->get('pedidos/(:num)', 'PedidoController::show/$1');
+    $routes->post('pedidos', 'PedidoController::create');
+    $routes->put('pedidos/(:num)', 'PedidoController::update/$1');
+    $routes->delete('pedidos/(:num)', 'PedidoController::delete/$1');
 
-// Rotas para CRUD de Itens de Pedido
-$routes->get('/itens-pedido', 'ItemPedidoController::index');
-$routes->get('/itens-pedido/(:num)', 'ItemPedidoController::show/$1');
-$routes->post('/itens-pedido', 'ItemPedidoController::create');
-$routes->put('/itens-pedido/(:num)', 'ItemPedidoController::update/$1');
-$routes->delete('/itens-pedido/(:num)', 'ItemPedidoController::delete/$1');
+    // Rotas para CRUD de Itens de Pedido
+    $routes->get('itens-pedido', 'ItemPedidoController::index');
+    $routes->get('itens-pedido/(:num)', 'ItemPedidoController::show/$1');
+    $routes->post('itens-pedido', 'ItemPedidoController::create');
+    $routes->put('itens-pedido/(:num)', 'ItemPedidoController::update/$1');
+    $routes->delete('itens-pedido/(:num)', 'ItemPedidoController::delete/$1');
+});
